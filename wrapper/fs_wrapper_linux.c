189433168e44b04976c4df648782b28398d6dc9d
valcheb <visel7@yandex.ru>
2020-04-22 16:39:57 +0300

    Some editorial changes

--- wrapper/fs_wrapper_linux.c
+++ wrapper/fs_wrapper_linux.c
@@ -1,11 +1,11 @@
 #include "fs_wrapper_linux.h"
 
-static FILE * file;
+static FILE *file;
 
 fswresult_e fs_wrapper_linux_opendir(fs_wrapper_dir_t *dir, const char *dirpath)
 {
     dir->data = opendir(dirpath);
-    if(dir->data == NULL)
+    if (dir->data == NULL)
         return FSWRESULT_ERROR;
     else
         return FSWRESULT_OK;
@@ -15,7 +15,7 @@ fswresult_e fs_wrapper_linux_open(const char *fname)
 {
     fclose(file);
     file = fopen(fname, "r"); // read only for compliance with fatpetit
-    if(file == NULL)
+    if (file == NULL)
         return FSWRESULT_ERROR;
     else
         return FSWRESULT_OK;
@@ -23,9 +23,9 @@ fswresult_e fs_wrapper_linux_open(const char *fname)
 
 fswresult_e fs_wrapper_linux_read(void *readbuf, uint16_t size, uint16_t *bread) //TODO: debug this
 {
-    *bread = (uint16_t)fread(readbuf,1,size,file);
+    *bread = (uint16_t)fread(readbuf, 1, size, file);
 
-    if(*bread != size)
+    if (*bread != size)
         return FSWRESULT_ERROR;
     else
         return FSWRESULT_OK;
