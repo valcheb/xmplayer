f73476f0e181005375f1d22119396121caebf23b
valcheb <visel7@yandex.ru>
2020-04-25 09:31:51 +0300

    Fix typo

--- wrapper/fs_wrapper_fatpetit.c
+++ wrapper/fs_wrapper_fatpetit.c
@@ -1,14 +1,14 @@
 #include "fs_wrapper_fatpetit.h"
 
 // TODO: debug all
-fswresult_e fs_wrapper_fatpetit_opendir(fs_wrapper_dir_t *dir, const char* dirpath)
+fswresult_e fs_wrapper_fatpetit_opendir(fs_wrapper_dir_t *dir, const char *dirpath)
 {
     DIR pf_dir;
     FRESULT res;
 
-    res = pf_opendir(&pf_dir,dirpath); //TODO: change pf_dir >>> dir->data
+    res = pf_opendir(&pf_dir, dirpath); //TODO: change pf_dir >>> dir->data
  
-    if(res == FR_OK)
+    if (res == FR_OK)
     {
         dir->data = pf_dir;
         return FSWRESULT_OK;
@@ -23,22 +23,22 @@ fswresult_e fs_wrapper_fatpetit_opendir(fs_wrapper_dir_t *dir, const char* dirpa
 fswresult_e fs_wrapper_fatpetit_readdir(fs_wrapper_dir_t *dir, fs_wrapper_diritem_t *item)
 {
     FILINFO fil;
-    FRESULT res = pf_readdir(&(dir->data),&fil);
+    FRESULT res = pf_readdir(&(dir->data), &fil);
 
-    if(res != FR_OK)
+    if (res != FR_OK)
         return FSWRESULT_ERROR;
 
-    strncpy(item->name,fil.fname,DIRITEM_NAME-1);
+    strncpy(item->name, fil.fname, DIRITEM_NAME-1);
     item->name[DIRITEM_NAME-1] = '\0';
 
     return FSWRESULT_OK;
 }
 
-fswresult_e fs_wrapper_linux_open(const char *fname)
+fswresult_e fs_wrapper_fatpetit_open(const char *fname)
 {
     FRESULT res = pf_open(fname);
 
-    if(res != FR_OK)
+    if (res != FR_OK)
         return FSWRESULT_ERROR;
     
     return FSWRESULT_OK;
--- wrapper/fs_wrapper_fatpetit.h
+++ wrapper/fs_wrapper_fatpetit.h
@@ -3,8 +3,8 @@
 
 #include "fs_wrapper_global.h"
 
-fswresult_e fs_wrapper_fatpetit_opendir(fs_wrapper_dir_t *dir, const char* dirpath);
+fswresult_e fs_wrapper_fatpetit_opendir(fs_wrapper_dir_t *dir, const char *dirpath);
 fswresult_e fs_wrapper_fatpetit_readdir(fs_wrapper_dir_t *dir, fs_wrapper_diritem_t *item);
-fswresult_e fs_wrapper_linux_open(const char *fname);
+fswresult_e fs_wrapper_fatpetit_open(const char *fname);
 
 #endif /*FS_WRAPPER_FATPETIT_H_*/
