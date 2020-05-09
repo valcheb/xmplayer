61a6c8add48380264c5c12c2f19edea78f2143d2
valcheb <visel7@yandex.ru>
2020-05-09 16:37:31 +0300

    Add functional macros

--- xmparser/xmparser.c
+++ xmparser/xmparser.c
@@ -2,6 +2,17 @@
 
 static xm_song_info_t song_info;
 
+#define SIZE_OF_ARRAY(arr) sizeof((arr))/sizeof((arr)[0])
+#define READ_VALUE(value) do {\
+    if (fs_access_ctx.read(&(value), sizeof((value))) != FSWRESULT_OK)\
+        return XMRESULT_ERROR;\
+} while(0);
+#define READ_STRING(string) do {\
+    if (fs_access_ctx.read((string), SIZE_OF_ARRAY((string))-1) != FSWRESULT_OK)\
+        return XMRESULT_ERROR;\
+} while(0);
+#define ADD_END_OF_STRING(string) (string)[SIZE_OF_ARRAY((string))-1] = '\0'
+
 xmresult_e xm_fill_main_header(xm_main_header_t *head)
 {
     if (head == NULL)
