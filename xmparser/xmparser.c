48b7c4c3ce50d863445de56701174a957e13d6b2
valcheb <visel7@yandex.ru>
2020-05-09 16:55:44 +0300

    Some editorial changes with macros

--- xmparser/xmparser.c
+++ xmparser/xmparser.c
@@ -3,6 +3,7 @@
 static xm_song_info_t song_info;
 
 #define SIZE_OF_ARRAY(arr) sizeof((arr))/sizeof((arr)[0])
+#define ADD_END_OF_STRING(string) (string)[SIZE_OF_ARRAY((string))-1] = '\0'
 #define READ_VALUE(value) do {\
     if (fs_access_ctx.read(&(value), sizeof((value))) != FSWRESULT_OK)\
         return XMRESULT_ERROR;\
@@ -10,8 +11,8 @@ static xm_song_info_t song_info;
 #define READ_STRING(string) do {\
     if (fs_access_ctx.read((string), SIZE_OF_ARRAY((string))-1) != FSWRESULT_OK)\
         return XMRESULT_ERROR;\
+    ADD_END_OF_STRING((string));\
 } while(0);
-#define ADD_END_OF_STRING(string) (string)[SIZE_OF_ARRAY((string))-1] = '\0'
 
 xmresult_e xm_fill_main_header(xm_main_header_t *head)
 {
@@ -32,10 +33,6 @@ xmresult_e xm_fill_main_header(xm_main_header_t *head)
     READ_VALUE(head->freq_table);
     READ_VALUE(head->default_tempo);
     READ_VALUE(head->default_bpm);
-
-    ADD_END_OF_STRING(head->idtext);
-    ADD_END_OF_STRING(head->module_name);
-    ADD_END_OF_STRING(head->tracker_name);
 
     return XMRESULT_OK;
 }
