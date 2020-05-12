47cff68229eca8b98371441eecd023c325421195
valcheb <visel7@yandex.ru>
2020-05-12 09:30:21 +0300

    Some editorial changes

--- xmparser/xmparser.c
+++ xmparser/xmparser.c
@@ -5,10 +5,10 @@ static xm_song_info_t song_info;
 #define XM_MAIN_HEADER_OFFSET         0
 #define XM_REST_OF_MAIN_HEADER_OFFSET 60
 #define XM_PATTERN_ORDER_TABLE_OFFSET 80
-#define SIZE_OF_ARRAY(arr) sizeof((arr))/sizeof((arr)[0])
-#define ADD_END_OF_STRING(string) (string)[SIZE_OF_ARRAY((string))-1] = '\0'
+#define SIZE_OF_ARRAY(arr) sizeof(arr)/sizeof((arr)[0])
+#define ADD_END_OF_STRING(string) (string)[SIZE_OF_ARRAY(string)-1] = '\0'
 #define READ_VALUE(value) do {\
-    if (fs_access_ctx.read(&(value), sizeof((value))) != FSWRESULT_OK)\
+    if (fs_access_ctx.read(&(value), sizeof(value)) != FSWRESULT_OK)\
         return XMRESULT_ERROR;\
 } while(0);
 
@@ -17,7 +17,7 @@ xmresult_e xm_read_main_header(xm_main_header_t *head)
     if (fs_access_ctx.seek(0) != FSWRESULT_OK)
         return XMRESULT_ERROR;
     
-    READ_VALUE(*(head));
+    READ_VALUE(*head);
 
     return XMRESULT_OK;
 }
@@ -27,7 +27,7 @@ xmresult_e xm_read_pattern_header(uint32_t offset, xm_pattern_header_t *phead)
     if (fs_access_ctx.seek(offset) != FSWRESULT_OK)
         return XMRESULT_ERROR;
 
-    READ_VALUE(*(phead));
+    READ_VALUE(*phead);
 
     return XMRESULT_OK;
 }
@@ -49,19 +49,19 @@ xmresult_e xm_read_sample_header(uint32_t offset, xm_sample_header_t *shead)
     if (fs_access_ctx.seek(offset) != FSWRESULT_OK)
         return XMRESULT_ERROR;
 
-    READ_VALUE(*(shead));
+    READ_VALUE(*shead);
 
     return XMRESULT_OK;
 }
 
 xmresult_e xm_fill_song_info(xm_song_info_t *song)
 {
-    if (xm_read_main_header(&(song->main_header)) != XMRESULT_OK)
+    if (xm_read_main_header(&song->main_header) != XMRESULT_OK)
         return XMRESULT_ERROR;
 
     song->pattern_order_table = XM_PATTERN_ORDER_TABLE_OFFSET;
     song->pattern_order_table_size = song->main_header.header_size - XM_PATTERN_ORDER_TABLE_OFFSET;
-    song->first_pattern = XM_REST_OF_MAIN_HEADER_OFFSET+song->main_header.header_size;
+    song->first_pattern = XM_REST_OF_MAIN_HEADER_OFFSET + song->main_header.header_size;
     if (xm_read_instruments(song) != XMRESULT_OK)
         return XMRESULT_ERROR;
 
@@ -72,19 +72,17 @@ xmresult_e xm_read_instruments(xm_song_info_t *song)
 {
     uint32_t current_offset = song->first_pattern;
     xm_pattern_header_t pattern_header;
-    int i;
 
-    for (i = 0; i < song->main_header.patterns_number; i++) //run through all pattern headers and datas
+    for (int i = 0; i < song->main_header.patterns_number; i++) //run through all pattern headers and datas
     {
         xm_read_pattern_header(current_offset, &pattern_header);
         current_offset += pattern_header.header_size + pattern_header.data_size;
     }
 
-    for (i = 0; i < song->main_header.instruments_number; i++)
+    for (int i = 0; i < song->main_header.instruments_number; i++)
     {
         song->instruments[i] = current_offset;
 
-        int s;
         xm_instrument_header_t instrument_header;
         if (xm_read_instrument_header(song->instruments[i], &instrument_header) != XMRESULT_OK)
             return XMRESULT_ERROR;
@@ -94,10 +92,10 @@ xmresult_e xm_read_instruments(xm_song_info_t *song)
         uint32_t samples_len = 0;
 
         current_offset += instrument_header.main_header.instrument_size; // move to first sample header in instrument
-        for (s = 0; s < samples_number; s++)
+        for (int s = 0; s < samples_number; s++)
         {   
             xm_sample_header_t sample_header;
-            if (xm_read_sample_header(current_offset,&sample_header) != XMRESULT_OK)
+            if (xm_read_sample_header(current_offset, &sample_header) != XMRESULT_OK)
                 return XMRESULT_ERROR;
 
             samples_len += sample_header.sample_length;
