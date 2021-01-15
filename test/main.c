afc1667101c518d20d43089c60686a700b674977
valcheb <cheb.valentin@gmail.com>
2021-01-15 14:27:55 +0300

    Rewrite test sequential reading of the order table

--- test/main.c
+++ test/main.c
@@ -138,130 +138,208 @@ void print_pattern_data(uint32_t offset)
     }
 }
 
-//flags
-static bool is_data_for_calc = true;
-static bool is_new_pattern_need = true;
-static bool is_new_row_need = true;
+/*
+    TODO рассуждения о частоте и времени
+    есть две таблицы частот.
+    как работает amiga - не понятно.
+    по линейной таблице вычисления представляют собой
 
-static void read_new_pattern(xm_pattern_header_t *pattern_header, uint16_t *order_table_pos, uint32_t *pattern_offset, xm_song_info_t *song)
-{
-    uint32_t order_table_offset = song->pattern_order_table + (uint32_t)*order_table_pos;
+    Period = 7680 - (Note * 64) - (FineTune / 2)
+    Frequency = 8363 * 2^((4608 - Period) / 768)
 
-    uint16_t pattern_index;
-    xm_read_pattern_index_in_order_table(order_table_offset, &pattern_index);
-    #if 1
-        printf("<<<Order_table_%d = pattern_%d>>>\n", *order_table_pos, pattern_index);
-    #endif
-    (*order_table_pos)++; //control limit of order table
+    где Note = PatternNote + RelativeTone
 
-    //uint32_t pattern_offset;
-    xm_read_pattern_offset(pattern_index, song->first_pattern, pattern_offset);
-    xm_read_pattern_header(*pattern_offset, pattern_header);
-    #if 1
-        printf("    <<<Pattern %d header info>>>\n", pattern_index);
-        printf("    hsize = %d, packtype = %d, rows = %d, dsize = %d\n",
-               pattern_header->header_size, pattern_header->packing_type,
-               pattern_header->rows_number, pattern_header->data_size);
-    #endif
-}
+    например
+    C-4
+    Note = 48 + 0 = 48;
+    Period = 7680 - 3072 - 0 = 4608
+    Frequency = 8363
+
+    Под Frequency здесь понимается (скорее всего)
+    скорость воспроизведения отчетов семпла.
+
+    Длина семпла влияет на результирующую частоту звука - чем короче семпл, тем выше звук
+    при прочих равных условиях.
+
+    Рассмотрим следующий момент.
+    Пусть BPM = 125, Speed = 25, Rate = 48000 (rate == частота дискретизации)
+    Количетсво строк = 10
+    Тогда tick_time (ms) = 2500 / BPM = 20 (ms)
+    Row_time = speed * tick_time = 500 (ms)
+    Pattern lenght = row_number * row_time = 5 (s)
+
+    Допустим нота C-4 -> частота отчетов 8363 о/с
+    За эти 5 секунд с такой частотой полностью воспроизведется семпл длиной 5*8363 = 41815
+
+    Но сколько отчетов будет для rate?
+    5 * 48000 = 240000
+    Как совместить Frequancy и rate?
+    По методу синтеза волновыми таблицами
+    Phase_increment = wave_lenght * freq_note / rate
+    Предполагаем, что Frequancy = wave_lenght * freq_note
+    получим, что
+    phase_increment = Frequancy / rate = 0,174229167
+    Данное утверждение нужно проверить
+
+    TODO рассуждения о инструменте
+    инструменты нумеруются от 1
+    Что значит, если мы считаем 0?
+    Предполагаю, что 0 - это не изменение инструмента.
+    Т.е. при чтении 0 нужно продолжать использовать выбранный ранее инструмент.
+    Так, на момент старта все инструменты должны быть инициированы, но замучены (note key off)
+
+*/
 
 typedef struct
 {
-    note_info_t notes[32]; //TODO define max
-    uint32_t    bread;
+    xm_pattern_header_t pattern_header;
+    uint32_t            pattern_offset;
+} pattern_data_t;
+
+#define MAX_SUPPORT_CHANNELS 32
+typedef struct
+{
+    note_info_t notes[MAX_SUPPORT_CHANNELS];
+    uint32_t    bytes_in_row;
 } row_info_t;
 
-static void read_new_row(row_info_t *note_row, uint32_t start_offset, uint16_t size)
+typedef struct
 {
-    //fs_access_ctx.seek(); TODO после чтения инструмента теряется оффсет, откуда читать новую строку
-    fs_access_ctx.seek(start_offset);
-    for (int chan_c = 0; chan_c < size; chan_c++)
-    {
-        xm_read_note(&note_row->notes[chan_c], &note_row->bread);
-    }
+    xm_instrument_header_t instrument_header;
+    xm_sample_header_t sample_header;
+    uint32_t sample_start_offset;
+    uint8_t note;
+    uint8_t instrument_number;
+    uint8_t volume; //TODO volume some difficult at this time, so simplify to constant
+    uint8_t effect;
+    uint8_t effect_parameter;
+} channel_state_t;
+
+static void read_new_pattern(pattern_data_t *pattern_data, uint16_t order_table_pos, xm_song_info_t *song)
+{
+    uint8_t pattern_index;
+
+    xm_read_pattern_index_in_order_table(song->pattern_order_table + order_table_pos,
+                                         &pattern_index);
+    xm_read_pattern_offset(pattern_index, song->first_pattern, &pattern_data->pattern_offset);
+    xm_read_pattern_header(pattern_data->pattern_offset, &pattern_data->pattern_header);
+
+    #if 1
+        printf("<<<Order_table_%d = pattern_%d>>>\n", order_table_pos, pattern_index);
+        printf("    <<<Pattern %d header info>>>\n", pattern_index);
+        printf("    hsize = %d, packtype = %d, rows = %d, dsize = %d\n",
+               pattern_data->pattern_header.header_size, pattern_data->pattern_header.packing_type,
+               pattern_data->pattern_header.rows_number, pattern_data->pattern_header.data_size);
+    #endif
 }
 
 static void clear_row(row_info_t *note_row, uint16_t size)
 {
     for (int chan_c = 0; chan_c < size; chan_c++)
     {
-        note_row->notes[chan_c].note = 0;
-        note_row->notes[chan_c].instrument = 0;
+        note_row->notes[chan_c].note               = 0;
+        note_row->notes[chan_c].instrument         = 0;
         note_row->notes[chan_c].volume_column_byte = 0;
-        note_row->notes[chan_c].effect_type = 0;
-        note_row->notes[chan_c].effect_parameter = 0;
-        note_row->bread = 0;
+        note_row->notes[chan_c].effect_type        = 0;
+        note_row->notes[chan_c].effect_parameter   = 0;
+        note_row->bytes_in_row                     = 0;
     }
 }
 
-/*
-TODO рассуждения о частоте и времени
-есть две таблицы частот.
-как работает amiga - не понятно.
-по линейной таблице вычисления представляют собой
-
-Period = 7680 - (Note * 64) - (FineTune / 2)
-Frequency = 8363 * 2^((4608 - Period) / 768)
-
-где Note = PatternNote + RelativeTone
-
-например
-C-4
-Note = 48 + 0 = 48;
-Period = 7680 - 3072 - 0 = 4608
-Frequency = 8363
-
-Под Frequency здесь понимается (скорее всего)
-скорость воспроизведения отчетов семпла.
-
-Длина семпла влияет на результирующую частоту звука - чем короче семпл, тем выше звук
-при прочих равных условиях.
-
-Рассмотрим следующий момент.
-Пусть BPM = 125, Speed = 25, Rate = 48000 (rate == частота дискретизации)
-Количетсво строк = 10
-Тогда tick_time (ms) = 2500 / BPM = 20 (ms)
-Row_time = speed * tick_time = 500 (ms)
-Pattern lenght = row_number * row_time = 5 (s)
-
-Допустим нота C-4 -> частота отчетов 8363 о/с
-За эти 5 секунд с такой частотой полностью воспроизведется семпл длиной 5*8363 = 41815
-
-Но сколько отчетов будет для rate?
-5 * 48000 = 240000
-Как совместить Frequancy и rate?
-По методу синтеза волновыми таблицами 
-Phase_increment = wave_lenght * freq_note / rate
-Предполагаем, что Frequancy = wave_lenght * freq_note
-получим, что 
-phase_increment = Frequancy / rate = 0,174229167
-Данное утверждение нужно проверить
-
-TODO рассуждения о инструменте
-инструменты нумеруются от 1
-Что значит, если мы считаем 0?
-Предполагаю, что 0 - это не изменение инструмента.
-Т.е. при чтении 0 нужно продолжать использовать выбранный ранее инструмент.
-Так, на момент старта все инструменты должны быть инициированы, но замучены (note key off)
-
-*/
-
-static void row_process(row_info_t *note_row, row_info_t *old_note_row, xm_song_info_t *song, uint32_t *buffer, uint32_t buffer_size)
+static void read_new_row(row_info_t *note_row, uint32_t row_offset, uint16_t chan_number)
 {
-    
-#if 0
-    printf("        ");
+    fs_access_ctx.seek(row_offset);
+    for (int chan_c = 0; chan_c < chan_number; chan_c++)
+    {
+        xm_read_note(&note_row->notes[chan_c], &note_row->bytes_in_row);
+    }
+
+    #if 1
+        printf("        ");
+        for (int chan_c = 0; chan_c < chan_number; chan_c++)
+        {
+            printf("|%d %d %d %d %d|", note_row->notes[chan_c].note,
+                   note_row->notes[chan_c].instrument,
+                   note_row->notes[chan_c].volume_column_byte,
+                   note_row->notes[chan_c].effect_type,
+                   note_row->notes[chan_c].effect_parameter);
+        }
+        printf("\n");
+    #endif
+}
+
+static void set_mute_channels(channel_state_t *channels_state, uint16_t chan_num)
+{
+    for (int chan_c = 0; chan_c < chan_num; chan_c++)
+    {
+        channels_state[chan_c].note = 97; // key off
+        channels_state[chan_c].instrument_number = 0;
+        channels_state[chan_c].sample_start_offset = 0;
+        channels_state[chan_c].volume = 64; // max volume
+        channels_state[chan_c].effect = 0;
+        channels_state[chan_c].effect_parameter = 0;
+    }
+}
+
+static void prepare_channels(row_info_t *note_row, channel_state_t *current_states, xm_song_info_t *song)
+{
     for (int chan_c = 0; chan_c < song->main_header.channels_number; chan_c++)
     {
-        printf("|%d %d %d %d %d|", note_row->notes[chan_c].note,
-               note_row->notes[chan_c].instrument,
-               note_row->notes[chan_c].volume_column_byte,
-               note_row->notes[chan_c].effect_type,
-               note_row->notes[chan_c].effect_parameter);
-    }
-    printf("\n");
-#endif
+        note_info_t new_note = note_row->notes[chan_c];
+        channel_state_t *current_state = &current_states[chan_c];
 
+        if (new_note.note != 0 && new_note.note != current_state->note) //new note coming
+        {
+            if (new_note.note == 97 || new_note.instrument < 1 || new_note.instrument > song->main_header.instruments_number)
+            {
+                current_state->note = 97; //duct tape: if you see an incorrect instrument just stop playing
+            }
+            else
+            {
+                if (new_note.instrument != current_state->instrument_number) //new instrument
+                {
+                    //get_instrument
+                    uint32_t instrument_offset = song->instruments[new_note.instrument - 1];
+                    xm_read_instrument_header(instrument_offset, &current_state->instrument_header);
+                    current_state->instrument_number = new_note.instrument;
+                    xm_instrument_main_header_t *instr_main_header = &current_state->instrument_header.main_header;
+                    xm_instrument_extra_header_t *instr_extra_header = &current_state->instrument_header.extra_header;
+
+                    #if 1
+                        printf("        ");
+                        printf("Name of instr%d = %.*s, offset = %d, size = %d ", new_note.instrument,
+                            (int)(sizeof(current_state->instrument_header.main_header.instrument_name)/sizeof(current_state->instrument_header.main_header.instrument_name[0])),
+                            current_state->instrument_header.main_header.instrument_name,
+                            song->instruments[new_note.instrument - 1], current_state->instrument_header.main_header.instrument_size);
+                        printf("\n");
+                    #endif
+
+                    //get_sample
+                    uint8_t sample_number_for_note = instr_extra_header->sample_number_for_notes[new_note.note];
+                    current_state->sample_start_offset = instrument_offset + instr_main_header->instrument_size; //here sample headers start
+
+                    for (int sample_c = 0; sample_c <= sample_number_for_note; sample_c++)
+                    {
+                        xm_read_sample_header(current_state->sample_start_offset, &current_state->sample_header);
+                        current_state->sample_start_offset += instr_extra_header->sample_header_size; // move to next sample header
+                    }
+
+                    #if 1
+                        printf("        ");
+                        printf("Sample number = %d", sample_number_for_note);
+                        printf("\n");
+                    #endif
+                }
+                current_state->note = new_note.note;
+                //get_freq
+                //save new state
+            }
+        }
+    }
+}
+
+static void row_process(row_info_t *current_note_row, row_info_t *old_note_row, uint16_t chan_num)
+{
     /*
         clear_calculated_buffer
         for all channels
@@ -270,110 +348,75 @@ static void row_process(row_info_t *note_row, row_info_t *old_note_row, xm_song_
             for all samples in tick (25 * rate / BPM / 10)
                 calculated[i] += cook_data_with_effect_type()
         normalize calculated by headroom
+        save in memory
     */
 
-//TODO проблема - читаем ноту - и что там? есть там нота или нет? повторение ли это предыдущего или пустота?
-    for (int chan_c = 0; chan_c < song->main_header.channels_number; chan_c++)
+    for (int chan_c = 0; chan_c < chan_num; chan_c++)
     {
-        xm_instrument_header_t instrument;
-        note_info_t note_info = note_row->notes[chan_c];
-        uint8_t instrument_pos = note_info.instrument;
-        
+        note_info_t current_note = current_note_row->notes[chan_c];
+        note_info_t old_note = old_note_row->notes[chan_c];
+
+        if (current_note.note != old_note.note)
+        {
+            //get_instr
+            //get_sample, clear phases
+            //get_note_freq
+            //calculate_increment
+        }
+        else if (current_note.instrument != current_note.instrument)
+        {
+            //get_instr
+            //get_sample, clear phases
+            //calculate_increment
+        }
+
+        //calculate_data(increment, sample, volume, effect, effect_type);
+
+
+        /*xm_instrument_header_t instrument;
+        note_info_t current_note = current_note_row->notes[chan_c];
+        uint8_t instrument_pos = current_note.instrument;
+
         if (instrument_pos > 0)
         {
-            uint32_t instrument_offset = song->instruments[instrument_pos - 1];
-            
-            xm_read_instrument_header(instrument_offset, &instrument);
-
-            //TODO возможно, если note_info.instrument == 0, это значит отсутствие звука или предыдущий инструмент
-            //TODO на старте old_row неплохо бы инициализировать note off
-
-            #if 1
-            printf("        ");
-            printf("Name of instr%d = %.*s, offset = %d, size = %d ", instrument_pos,
-                (int)(sizeof(instrument.main_header.instrument_name)/sizeof(instrument.main_header.instrument_name[0])),
-                instrument.main_header.instrument_name,
-                song->instruments[instrument_pos - 1], instrument.main_header.instrument_size);
-            printf("\n");
-            #endif
+            read_new_instrument();
         }
-        /*
         else
         {
-            //use instrument from previous row ???
-        }
-        */
 
-        //uint8_t sample_number_for_note = instrument.extra_header.sample_number_for_notes[note_info.note];
-        //uint32_t sample_offset = instrument_offset;
+        }*/
     }
 }
 
-static void calculate_buffer3(xm_song_info_t *song, uint32_t *buffer, uint32_t buffer_size)
+static void test_play(xm_song_info_t *song)
 {
-    static xm_pattern_header_t pattern_header;
-    static uint16_t order_table_pos = 0;
-    static uint32_t pattern_offset = 0;
-    static uint32_t row_counter = 0;
-    static uint32_t row_pos = 0;
-    static row_info_t new_row;
-    static row_info_t old_row;
+    pattern_data_t current_pattern;
+    uint16_t order_table_pos = 0;
+    uint16_t chan_num = song->main_header.channels_number;
+    row_info_t new_row;
+    channel_state_t channels_states[chan_num];
+    set_mute_channels(channels_states, chan_num); //set default volume, key off
 
-    if (order_table_pos >= song->main_header.song_len && row_counter == 0) //TODO что же является признаком
+    while (order_table_pos < song->main_header.song_len)
     {
-        is_data_for_calc = false;
-        return ;
-    }
+        read_new_pattern(&current_pattern, order_table_pos, song);
 
-    if (is_new_pattern_need == true)
-    {
-        read_new_pattern(&pattern_header, &order_table_pos, &pattern_offset, song);
-        is_new_pattern_need = false;
-    }
+        uint16_t row_counter      = 0;
+        uint32_t row_pos          = 0;
+        uint16_t max_row          = current_pattern.pattern_header.rows_number;
+        uint32_t start_row_offset = current_pattern.pattern_offset + current_pattern.pattern_header.header_size;
 
-    if (is_new_row_need == true)
-    {
-        //prepare_new_row(new_row, offset, size);
-        //prepare_channels(new_row, channels);
-        /*
-        channel: frequance
-                 sample_offset //for dynamic read if sample length larger than size of buffer
-                 sample_buffer[buffer_size]
-                 sample_full_size
-                 current_sample_phase //index in sample_buffer
-                 effect 
-
-        */
-        old_row = new_row;
-        clear_row(&new_row, song->main_header.channels_number);
-        read_new_row(&new_row, pattern_offset + pattern_header.header_size + row_pos, song->main_header.channels_number);
-        row_pos += new_row.bread;
-        is_new_row_need = false;
-    }
-
-    if (is_new_pattern_need == false && is_new_row_need == false)
-    {
-        row_process(&new_row, &old_row, song, buffer, buffer_size); //process notes of row
-        row_counter++;
-        is_new_row_need = true;
-
-        if (row_counter >= pattern_header.rows_number)
+        while (row_counter < max_row)
         {
-            row_counter = 0;
-            row_pos = 0;
-            is_new_pattern_need = true;
-        }
-    }
-}
+            clear_row(&new_row, chan_num);
+            read_new_row(&new_row, start_row_offset + row_pos, chan_num);
+            prepare_channels(&new_row, channels_states, song);
 
-#define RATE 48000
-static void test_play_3(xm_song_info_t *song)
-{
-    uint32_t calc_size = 25 * RATE / song->main_header.default_bpm / 10;
-    uint32_t calculated[calc_size]; //25 * rate / BPM / 10
-    while(is_data_for_calc == true)
-    {
-        calculate_buffer3(song, calculated, calc_size);
+            row_counter++;
+            row_pos += new_row.bytes_in_row;
+        }
+
+        order_table_pos++;
     }
 }
 
@@ -406,9 +449,9 @@ int main()
     print_patterns_headers(song_info.first_pattern, song_info.main_header.patterns_number);
     printf("<<<Notes>>>\n");
     print_pattern_data(song_info.first_pattern);*/
-    
 
-   test_play_3(&song_info);
+
+   test_play(&song_info);
 
     return 0;
 }
